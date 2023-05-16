81c7c1aa039e69af7fec9919696199bdedc662aa
sjyoung428 <sjy8267@naver.com>
2023-05-17 00:35:44 +0900

    feat: prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import authRouter from "./routes/auth.route";
 
 const app = express();
 const logger = morgan("dev");
@@ -8,5 +9,7 @@ const logger = morgan("dev");
 app.use(express.json());
 app.use(cors());
 app.use(logger);
+
+app.use("/auth", authRouter);
 
 export default app;
---
+++ src/middlewares/validateToken.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+export const validateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(StatusCodes.BAD_REQUEST).send("Token is missing");
+  }
+  next();
+};
