0851dfd3e280e2906b64201c9912fe4175840ac1
sjyoung428 <sjy8267@naver.com>
2023-06-07 22:06:34 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import mqtt from "mqtt";
 import morgan from "morgan";
 import cors from "cors";
 import authRouter from "./routes/auth.route";
@@ -11,8 +10,6 @@ const MQTT = process.env.MQTT || "";
 
 const app = express();
 const logger = morgan("dev");
-
-export const client = mqtt.connect(MQTT);
 
 app.use(cors());
 app.use(express.json());
--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -1,14 +1,10 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { client } from "../app";
-
-const TOPIC = process.env.TOPIC || "";
 
 export const feed = async (req: Request, res: Response) => {
   //const response =  await api.post("/feed");
   //const { data } = response;
   //console.log(data);
-  client.publish(TOPIC, "ON");
 
   return res.status(StatusCodes.ACCEPTED).json({
     message: "버튼 눌림",
