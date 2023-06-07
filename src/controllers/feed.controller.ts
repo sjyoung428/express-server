85fbf8b640d9a1b40e7f004cd042d9dfc797b631
sjyoung428 <sjy8267@naver.com>
2023-06-07 21:16:16 +0900

    payload 수정

--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -8,7 +8,7 @@ export const feed = async (req: Request, res: Response) => {
   //const response =  await api.post("/feed");
   //const { data } = response;
   //console.log(data);
-  client.publish(TOPIC, "1");
+  client.publish(TOPIC, "ON");
 
   return res.status(StatusCodes.ACCEPTED).json({
     message: "버튼 눌림",
