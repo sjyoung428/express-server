7f579e6c885103a2a1d50ddd090f2f7633e2c17f
sjyoung428 <sjy8267@naver.com>
2023-06-07 21:07:41 +0900

    아두이노

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mqtt from "mqtt";
 import morgan from "morgan";
 import cors from "cors";
 import authRouter from "./routes/auth.route";
@@ -6,8 +7,12 @@ import userRouter from "./routes/user.route";
 import feedRouter from "./routes/feed.route";
 import { validateToken } from "./middlewares/validateToken";
 
+const MQTT = process.env.MQTT || "";
+
 const app = express();
 const logger = morgan("dev");
+
+export const client = mqtt.connect(MQTT);
 
 app.use(cors());
 app.use(express.json());
--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { api } from "../api/axios.instance";
+import { client } from "../app";
+
+const TOPIC = process.env.TOPIC || "";
 
 export const feed = async (req: Request, res: Response) => {
   //const response =  await api.post("/feed");
   //const { data } = response;
   //console.log(data);
+  client.publish(TOPIC, "1");
 
   return res.status(StatusCodes.ACCEPTED).json({
     message: "버튼 눌림",
