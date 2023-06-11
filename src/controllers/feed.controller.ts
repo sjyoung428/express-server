cc877ee6cf7b7b0db466d5136dc6782cbc178aa6
sjyoung428 <sjy8267@naver.com>
2023-06-12 00:10:53 +0900

    fix

--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -3,9 +3,13 @@ import { StatusCodes } from "http-status-codes";
 import { api } from "../api/axios.instance";
 
 export const feed = async (req: Request, res: Response) => {
-  const response = await api.post("/post?message=TEST_MESSAGE");
-  const { data } = response;
-  console.log(data);
+  try {
+    const response = await api.post("/post?message=TEST_MESSAGE");
+    const { data } = response;
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
 
   return res.status(StatusCodes.ACCEPTED).json({
     message: "버튼 눌림",
