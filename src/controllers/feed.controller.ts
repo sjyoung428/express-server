18b5dd27c6e1b960ea68c095d6d2f42582a0d539
sjyoung428 <sjy8267@naver.com>
2023-06-28 19:02:38 +0900

    test

--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -3,15 +3,11 @@ import { StatusCodes } from "http-status-codes";
 import { api } from "../api/axios.instance";
 
 export const feed = async (req: Request, res: Response) => {
-  try {
-    const response = await api.post("/feed");
-    const { data } = response;
-    console.log(data);
-  } catch (err) {
-    console.error(err);
-  }
+  const response = await api.post("/feed");
+  const { data } = response;
+  console.log(data);
 
   return res.status(StatusCodes.ACCEPTED).json({
-    message: "버튼 눌림",
+    message: `버튼 눌림 ${data} `,
   });
 };
